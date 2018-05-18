75a325a537d69c0d56b0752c5be2bc4fd5665930
Ben Gotthold <bengotthold@gmail.com>
2018-05-18 15:37:20 -0600

    before bg change

--- src/home.js
+++ src/home.js
@@ -1,33 +1,29 @@
-
-
-
-
 function init(){
    
-    bGImage()
-    navBar()
-    toggleBtn()
- 
+    bGImage();
+    toggleBtn();
+
+    $(".navbar a, footer a[href='#home']").on('click', function(event) {
+     
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){
+          window.location.hash = hash;
+        });
+    });
+    var feed = new Instafeed({
+        //get: 'tagged',
+        //tagName: 'iceclimbing',
+        clientId: '111680786b694bc1b47154dba7ffbfbe',
+        accessToken: '3522981484.1116807.ac98862469174adab8a7741c5c29a2fa',
+        userId:'3522981484',
+        get: 'user'
+    });
+    feed.run();
 }
 
-// Handel active link switching and loading the right active nav-item
-function navBar(){
-
-    let loc = location.hash;
-    $(".navItems").find(".active").removeClass("active");
-    $('.navItems .nav-link').each(function(){
-        var $this = $(this);
-        if($this.attr('href').indexOf(loc) !== -1){
-            $this.addClass('active');
-        }
-    })
-
-    $(".navItems .nav-link").on("click", function(){
-        $(".navItems").find(".active").removeClass("active");
-        $(this).addClass("active");
-     });
-
-}
 
 // set the correct bg image based on screen size
 // Put the banner in the correct place
@@ -50,12 +46,24 @@ function bGImage(){
     }
     $(".bg").css("margin-top", -30 + "px");
 
-    let right = 0
-    if(docWidth > $(".bg").width() ){
-        right += 0.5 * (docWidth - $(".bg").width())
-    }
-    $(".craig-banner").css("right", right + 20 + "px")
-    $(".craig-banner").css("top", navHeight + 20 + "px")
+    // let right = 0
+    // if(docWidth > $(".bg").width() ){
+    //     right += 0.5 * (docWidth - $(".bg").width())
+    // }
+    // $(".craig-banner").css("right", right + 20 + "px")
+    // $(".craig-banner").css("top", navHeight + 20 + "px")
+
+    let left = 0
+    //if(docWidth > $(".bg").width() ){
+        left += docWidth
+        left -=  $(".craig-banner img").width()
+        left -= (0.5 * (docWidth - $(".bg").width()))
+   // }
+    let top = 0
+    top -= $(".bg").height()
+    $(".craig-banner").css("margin-left", left - 20 +"px")
+    $(".craig-banner").css("margin-top", top + navHeight  + "px")
+    
 
 }
 
@@ -73,9 +81,12 @@ function toggleBtn(){
 }
 
 
-
+$(document).ready(function(){
+    init()
+})
+$(window).on('activate.bs.scrollspy', function (e) {
+    history.replaceState({}, "",  $('.nav-item .active').attr("href"));
+});
 $(window).resize(function() {
     init()
-  });
-
-init()
+});
