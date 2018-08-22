3c53cda085a1b2333feee156a17833b63975e9f9
Ben Gotthold <bengotthold@gmail.com>
2018-08-21 22:13:57 -0600

    style changes for large photo viewer

--- src/photo.js
+++ src/photo.js
@@ -7,7 +7,7 @@ let options = {
     clientId: 'bed1cc4277c84daca3d28897c78c29a5',
     accessToken: '253757049.bed1cc4.2992b18b8a814342a4ff03d8166b6d56',
     get: 'user',
-    userId:'253757049',
+    userId: '253757049',
     // get:'tagged',
     // tagName:'arcteryx',
 
@@ -18,19 +18,19 @@ let thumbTemplate = `<a class="thumbSlide"><img src="{{image}}" /></a>`
 
 let fullTemplate = `<div class="instaSlide fade">
 <div class="numbertext" >{{model.created_time}}</div>
-<img src={{image}} style="margin: auto; max-width: 90%; max-height: 75vh;">
+<img src={{image}} style="margin: auto; max-width: 90%; max-height: 75vh; border-radius: 5px; pointer-events:none">
 <!--<div class="caption-container">
 <p>{{caption}}</p>
 </div> -->
 </div>`
 
-function instaFeed(limit){
+function instaFeed(limit) {
     limit = limit
     let thumbOptions = jQuery.extend({}, options)
     thumbOptions.limit = limit ? limit : 16
     thumbOptions.target = 'thumbnailFeed'
     thumbOptions.template = thumbTemplate
-    thumbOptions.after = function(){ numberSlides()} 
+    thumbOptions.after = function () { numberSlides() }
     let thumbnailFeed = new Instafeed(thumbOptions);
     thumbnailFeed.run();
 
@@ -39,77 +39,77 @@ function instaFeed(limit){
     fullOptions.template = fullTemplate
     fullOptions.resolution = 'standard_resolution'
     fullOptions.target = 'fullInstaSlideShow'
-    fullOptions.after = function(){ addDots();openModal();currentInstaSlide(instaSlideIndex);}
+    fullOptions.after = function () { addDots(); openModal(); currentInstaSlide(instaSlideIndex); }
     fullFeed = new Instafeed(fullOptions);
 }
 
 
 
-function numberSlides(){
+function numberSlides() {
 
     let w = $(window).width()
     let h = $(window).height()
-    centerInsta(w,h);
+    centerInsta(w, h);
 
-    $(".thumbSlide").each(function( index ) {
-        $(this).click(function(){
-            if(!instaFeedRunning){
+    $(".thumbSlide").each(function (index) {
+        $(this).click(function () {
+            if (!instaFeedRunning) {
                 instaFeedRunning = true;
                 instaSlideIndex = index
                 fullFeed.run()
             }
-            else{
+            else {
                 openModal();
                 currentInstaSlide(index)
             }
-            
+
         })
-       
+
     })
 }
 
-function addDots(){
-    $(".instaSlide").each(function( index ) {
+function addDots() {
+    $(".instaSlide").each(function (index) {
         let html = `  <span class="dot instaDot" onclick="currentInstaSlide(${index})"></span>`
         $("#instaDots").append(html)
         let rawTime = $(this).find(".numbertext").html()
         $(this).find(".numbertext").html(formatDate(rawTime))
     })
 
-    $("#fullInstaSlideShow img, #fullInstaSlideShow p").on("swiped-left", function(){
+    $("#fullInstaSlideShow img, #fullInstaSlideShow p").on("swiped-left", function () {
         plusInstaSlides(1)
     })
-    
-    $("#fullInstaSlideShow img, #fullInstaSlideShow p").on("swiped-right", function(){
+
+    $("#fullInstaSlideShow img, #fullInstaSlideShow p").on("swiped-right", function () {
         plusInstaSlides(-1)
     })
 }
 
-function formatDate(d){
+function formatDate(d) {
     d = parseInt(d)
-    let date = new Date(d*1000);
+    let date = new Date(d * 1000);
 
-		m = date.getMonth();
-		d = date.getDate();
-		y = date.getFullYear();
+    m = date.getMonth();
+    d = date.getDate();
+    y = date.getFullYear();
 
-		let month_names = new Array ( );
-		month_names[month_names.length] = "Jan";
-		month_names[month_names.length] = "Feb";
-		month_names[month_names.length] = "Mar";
-		month_names[month_names.length] = "Apr";
-		month_names[month_names.length] = "May";
-		month_names[month_names.length] = "Jun";
-		month_names[month_names.length] = "Jul";
-		month_names[month_names.length] = "Aug";
-		month_names[month_names.length] = "Sep";
-		month_names[month_names.length] = "Oct";
-		month_names[month_names.length] = "Nov";
-		month_names[month_names.length] = "Dec";
+    let month_names = new Array();
+    month_names[month_names.length] = "Jan";
+    month_names[month_names.length] = "Feb";
+    month_names[month_names.length] = "Mar";
+    month_names[month_names.length] = "Apr";
+    month_names[month_names.length] = "May";
+    month_names[month_names.length] = "Jun";
+    month_names[month_names.length] = "Jul";
+    month_names[month_names.length] = "Aug";
+    month_names[month_names.length] = "Sep";
+    month_names[month_names.length] = "Oct";
+    month_names[month_names.length] = "Nov";
+    month_names[month_names.length] = "Dec";
 
-		let thetime = month_names[m] + ' ' + d + ' ' + y;
+    let thetime = month_names[m] + ' ' + d + ' ' + y;
 
-		return thetime;
+    return thetime;
 
 }
 
@@ -125,8 +125,8 @@ function showInstaSlides(n) {
     var i;
     var slides = document.getElementsByClassName("instaSlide");
     var dots = document.getElementsByClassName("instaDot");
-    if (n > slides.length-1) { instaSlideIndex = 0 }
-    if (n < 0) { instaSlideIndex = slides.length -1}
+    if (n > slides.length - 1) { instaSlideIndex = 0 }
+    if (n < 0) { instaSlideIndex = slides.length - 1 }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -142,18 +142,16 @@ function openModal() {
 
     document.getElementById('fullInstaSlideShowModal').style.display = "block";
     $(".wrapper").addClass("modal-open");
-    setTimeout(function(){ $(".nav-link").removeClass("active") }, 100);
+    setTimeout(function () { $(".nav-link").removeClass("active") }, 100);
 
-  }
-  
-  function closeModal(scrollFlag) {
+}
+
+function closeModal(scrollFlag) {
     document.getElementById('fullInstaSlideShowModal').style.display = "none";
     $(".wrapper").removeClass("modal-open")
-    if(!scrollFlag){
+    if (!scrollFlag) {
         location.hash = "#photos";
         let y = $(window).scrollTop();
-        $(window).scrollTop(y-100);
+        $(window).scrollTop(y - 100);
     }
-    
-  }
-
+}
